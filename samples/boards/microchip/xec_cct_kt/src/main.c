0ce9e3a8712c20213729ad6fc3d7b3b5143a48d5
Scott Worley <scott.worley@microchip.com>
2026-08-25 13:24:20 -0400

    samples: boards: microchip: xec_cct_kt: Sleep kernel timer example
    We created a sample project using the CCT-LPM based kernel timer
    driver. The sample loops trigger PM sleep for 3 seconds.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- samples/boards/microchip/xec_cct_kt/src/main.c
+++ samples/boards/microchip/xec_cct_kt/src/main.c
@@ -5,11 +5,17 @@
  */
 
 #include <soc.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/pinctrl.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-#include <zephyr/drivers/pinctrl.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/logging/log_ctrl.h>
 
 #include "xec_vci.h"
+
+LOG_MODULE_REGISTER(app, CONFIG_LOG_DEFAULT_LEVEL);
 
 #define SLEEP_MS   3000U
 #define ITERATIONS 0
@@ -25,6 +31,11 @@
 PINCTRL_DT_DEFINE(APP_PINS_NODE);
 static const struct pinctrl_dev_config *app_pins_pcfg = PINCTRL_DT_DEV_CONFIG_GET(APP_PINS_NODE);
 
+#define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
+
+static const struct gpio_dt_spec pm_ds_pin =
+	GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, pm_mchp_deep_sleep_marker_gpios);
+
 /*
  * Exercise the CCT kernel timer across tickless sleeps. Each iteration sleeps
  * long enough to let the PM policy enter a low-power state (the board's
@@ -38,17 +49,26 @@ int main(void)
 	uint32_t i = 0, r = 0;
 	int ret = 0;
 
-	printk("XEC CCT kernel timer sample\n");
-	printk("system timer HW cycles/sec: %u\n", sys_clock_hw_cycles_per_sec());
-	printk("ticks/sec: %d\n", CONFIG_SYS_CLOCK_TICKS_PER_SEC);
+	LOG_INF("XEC CCT kernel timer sample");
+	LOG_INF("system timer HW cycles/sec: %u", sys_clock_hw_cycles_per_sec());
+	LOG_INF("ticks/sec: %d", CONFIG_SYS_CLOCK_TICKS_PER_SEC);
 
 	/* Route the test clock to GPIO060 for scope measurement. */
 	ret = pinctrl_apply_state(app_pins_pcfg, PINCTRL_STATE_DEFAULT);
 	if (ret) {
-		printk("app-pin-list pinctrl apply failed: %d\n", ret);
+		LOG_INF("app-pin-list pinctrl apply failed: %d", ret);
 	} else {
-		printk("test clock routed to GPIO060 (AF2)\n");
-		printk("VCI_IN1 pin function enabled");
+		LOG_INF("test clock routed to GPIO060 (AF2)");
+		LOG_INF("VCI_IN1 pin function enabled");
+	}
+
+	if (gpio_is_ready_dt(&pm_ds_pin)) {
+		ret = gpio_pin_configure_dt(&pm_ds_pin, GPIO_OUTPUT_HIGH);
+		if (ret != 0) {
+			LOG_ERR("PM deep sleep pin config error (%d)", ret);
+		}
+	} else {
+		LOG_ERR("PM deep sleep pin not ready!");
 	}
 
 	mchp_vci_in_input_enable(BIT(MEC_VCI_IN1_POS));
@@ -56,13 +76,18 @@ int main(void)
 	mchp_vci_edge_detect_clr_all();
 	mchp_vci_in_latch_reset(BIT(MEC_VCI_IN1_POS));
 
-	printk("Disconnect Debugger and press switch S4 when ready");
+	LOG_INF("Disconnect Debugger and press switch S4 when ready");
+	log_flush();
+
 	r = mchp_vci_pedge_detect() & mchp_vci_nedge_detect();
 	while ((r & BIT(MEC_VCI_IN1_POS)) == 0) {
 		r = mchp_vci_pedge_detect() & mchp_vci_nedge_detect();
 	}
 
-	printk("Button S4 pressed. Disabling Debug interface");
+	LOG_INF("Button S4 pressed. Disabling Debug interface");
+	log_flush();
+
+#if 0
 	sys_write32(0, XEC_ECS_BASE + XEC_ECS_DBG_CR_OFS);
 	sys_write32(0, 0xE000EDFCU);
 	sys_write32(0xA05F0000U, 0xE0000EDFU);
@@ -74,6 +99,7 @@ int main(void)
 
 	r = sys_read32(XEC_ECS_BASE + XEC_ECS_DBG_CR_OFS);
 	printk("ECS reg 0x%08x = 0x%08x", (XEC_ECS_BASE + XEC_ECS_DBG_CR_OFS), r);
+#endif
 
 #if ITERATIONS == 0
 	while (true) {
@@ -92,23 +118,26 @@ int main(void)
 		/* Unsigned subtraction is wrap-correct for the 32-bit counter. */
 		uint32_t dcyc = cyc1 - cyc0;
 
-		printk("iter %u: requested %u ms, uptime +%lld ms, "
-		       "cycles +%u (%u ms by cycle count)\n",
-		       i, SLEEP_MS, (long long)(up1 - up0), dcyc, k_cyc_to_ms_near32(dcyc));
-
+		LOG_INF("iter %u: requested %u ms, uptime +%lld ms, "
+			"cycles +%u (%u ms by cycle count)",
+			i, SLEEP_MS, (long long)(up1 - up0), dcyc, k_cyc_to_ms_near32(dcyc));
+			
+#if 0 /* uncomment if XEC_SOC_PM_CLK_REQ_DEBUG is uncommented in soc_pm_mgmt.c */
 		r = sys_read32(XEC_VBAT_MEM_BASE);
-		printk("PCR CLK_REQ0 = 0x%08x", r);
+		LOG_INF("PCR CLK_REQ0 = 0x%08x", r);
 		r = sys_read32(XEC_VBAT_MEM_BASE + 4U);
-		printk("PCR CLK_REQ1 = 0x%08x", r);
+		LOG_INF("PCR CLK_REQ1 = 0x%08x", r);
 		r = sys_read32(XEC_VBAT_MEM_BASE + 8U);
-		printk("PCR CLK_REQ2 = 0x%08x", r);
+		LOG_INF("PCR CLK_REQ2 = 0x%08x", r);
 		r = sys_read32(XEC_VBAT_MEM_BASE + 0xCU);
-		printk("PCR CLK_REQ3 = 0x%08x", r);
+		LOG_INF("PCR CLK_REQ3 = 0x%08x", r);
 		r = sys_read32(XEC_VBAT_MEM_BASE + 0x10U);
-		printk("PCR CLK_REQ4 = 0x%08x", r);
+		LOG_INF("PCR CLK_REQ4 = 0x%08x", r);
+#endif
+		log_flush();
 	}
 
-	printk("Sample complete\n");
+	LOG_INF("Sample complete");
 
 	return 0;
 }
